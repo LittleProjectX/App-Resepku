1754bb965020e76308103880d36862cb94f54e2d
LittleProjectX <littleproject342@gmail.com>
2026-08-02 22:09:52 +0700

    set app icon & release

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:seleraku/app/core/theme/app_theme.dart';
 import 'package:seleraku/app/core/utils/page_transition.dart';
 import 'package:seleraku/firebase_options.dart';
@@ -10,7 +9,6 @@ import 'app/routes/app_pages.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await GetStorage.init();
   runApp(MyApp());
 }
 
